c245ca1b56397f30196108df6bf4794a01c2ba06
sansna <1185280650@qq.com>
2020-08-01 18:59:17 +0800

    @Wentao[master]: update more - sansna

--- tmpl/tmpl.py
+++ tmpl/tmpl.py
@@ -5,7 +5,7 @@
 
 #import os
 #import sys 
-#sys.path.append(os.path.abspath("../../"))
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 import time
 
 now = int(time.time())
